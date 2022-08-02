d110ee87c360f7cee65f8de0b28814dbe083ce0c
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-08-02 19:51:28 +0300

    phone numb update

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,12 +3,12 @@ import './contact.css'
 const contact = () => {
   return (
     <div className='contact'>
-      <a href="tel:+6199942413">
+      <a href="tel:05537462928">
         <span>☎️</span>
         <p>Özlem'i ara</p>
       </a>
 
-      <a href="tel:+6199942413">
+      <a href="tel:05464475268">
         <span>☎️</span>
         <p>Tuna'yı ara</p>
       </a>
